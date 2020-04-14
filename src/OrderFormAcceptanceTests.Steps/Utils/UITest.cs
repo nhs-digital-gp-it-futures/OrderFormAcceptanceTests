de4dada3d99e416d2b31443d46905a853a0540aa
Tony Lawson <tony.lawson@bjss.com>
2020-04-14 16:00:16 +0100

    added skeleton projects and utils

---
+++ src/OrderFormAcceptanceTests.Steps/Utils/UITest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OrderFormAcceptanceTests.Actions;
+using OrderFormAcceptanceTests.Actions.Collections;
+
+namespace OrderFormAcceptanceTests.Steps.Utils
+{
+    public sealed class UITest
+    {
+        internal string ConnectionString;
+        internal IWebDriver Driver;
+        internal PageActionCollection Pages;
+        internal readonly string Url;
+
+        public UITest()
+        {
+            ConnectionString = EnvironmentVariables.DbConnectionString();
+
+            Driver = new BrowserFactory().Driver;
+            Pages = new PageActions(Driver).PageActionCollection;
+            Url = EnvironmentVariables.Url();
+
+            GoToUrl();
+        }
+
+        public void GoToUrl()
+        {
+            Driver.Navigate().GoToUrl(Url);
+        }
+    }
+}
