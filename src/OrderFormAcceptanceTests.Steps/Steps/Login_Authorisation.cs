e37f3b37fffcf056655b83b7d4f045b7134b4f82
Jonathan Alsop <j.r.alsop.18@gmail.com>
2020-05-05 14:55:55 +0100

    Added Scenarios and step outlines

---
+++ src/OrderFormAcceptanceTests.Steps/Steps/Login_Authorisation.cs
@@ -0,0 +1,67 @@
+﻿using OrderFormAcceptanceTests.Steps.Utils;
+using TechTalk.SpecFlow;
+
+namespace OrderFormAcceptanceTests.Steps.Steps
+{
+    [Binding]
+    public class Login_Authorisation : TestBase
+    {
+        public Login_Authorisation(UITest test, ScenarioContext context) : base(test, context)
+        {
+        }
+
+        [Given(@"that a User is not authenticated and the user  logs in using the '(.*)' function")]
+        public void GivenThatAUserIsNotAuthenticatedAndTheUserLogsInUsingTheFunction(string p0)
+        {
+            Context.Pending();
+        }
+        
+        [Given(@"that a User is not authenticated and the user selects the order form tile")]
+        public void GivenThatAUserIsNotAuthenticatedAndTheUserSelectsTheOrderFormTile()
+        {
+            Context.Pending();
+        }
+        
+        [Given(@"the User is prompted to login")]
+        public void GivenTheUserIsPromptedToLogin()
+        {
+            Context.Pending();
+        }
+        
+        [When(@"the User is a Buyer User")]
+        public void WhenTheUserIsABuyerUser()
+        {
+            Context.Pending();
+        }
+        
+        [When(@"the User is not a Buyer User")]
+        public void WhenTheUserIsNotABuyerUser()
+        {
+            Context.Pending();
+        }
+        
+        [Then(@"the Buyer will be logged in")]
+        public void ThenTheBuyerWillBeLoggedIn()
+        {
+            Context.Pending();
+        }
+        
+        [Then(@"the Buyer will be able to access the Order Form feature without having to authenticate again")]
+        public void ThenTheBuyerWillBeAbleToAccessTheOrderFormFeatureWithoutHavingToAuthenticateAgain()
+        {
+            Context.Pending();
+        }
+        
+        [Then(@"will be taken to the Order Form Feature")]
+        public void ThenWillBeTakenToTheOrderFormFeature()
+        {
+            Context.Pending();
+        }
+        
+        [Then(@"the User will be informed they cannot access that feature")]
+        public void ThenTheUserWillBeInformedTheyCannotAccessThatFeature()
+        {
+            Context.Pending();
+        }
+    }
+}
---
+++ src/OrderFormAcceptanceTests.Tests/Features/LoginAuthorisationOrderForm.feature
@@ -0,0 +1,23 @@
+﻿Feature: Login - Authorisation - Order Form
+	As a Buyer
+	I want to to be authorised to use the Order Form
+	So that I can use the Order Form if I have permission
+
+Scenario: Login as a Buyer via Login
+	Given that a User is not authenticated and the user  logs in using the 'login' function
+	When the User is a Buyer User
+	Then the Buyer will be logged in
+	And the Buyer will be able to access the Order Form feature without having to authenticate again
+
+Scenario: Login as a Buyer via Tile
+	Given that a User is not authenticated and the user selects the order form tile
+	And the User is prompted to login
+	When the User is a Buyer User
+	Then the Buyer will be logged in
+	And will be taken to the Order Form Feature
+
+Scenario: Login as via Tile but not a buyer
+	Given that a User is not authenticated and the user selects the order form tile
+	And the User is prompted to login
+	When the User is not a Buyer User
+	Then the User will be informed they cannot access that feature
