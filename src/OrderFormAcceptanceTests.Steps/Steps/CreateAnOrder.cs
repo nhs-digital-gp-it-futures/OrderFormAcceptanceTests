46ae0032166cffafb3e7d96553928be998185f0c
Jonathan Alsop <j.r.alsop.18@gmail.com>
2020-05-11 14:50:09 +0100

    Add Create An Order AC tests

---
+++ src/OrderFormAcceptanceTests.Steps/Steps/CreateAnOrder.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using OrderFormAcceptanceTests.Steps.Utils;
+using TechTalk.SpecFlow;
+
+namespace OrderFormAcceptanceTests.Steps.Steps
+{
+    [Binding]
+    public class CreateAnOrder : TestBase
+    {
+        public CreateAnOrder(UITest test, ScenarioContext context) : base(test, context)
+        {
+        }
+
+        [Given(@"the User is presented with the Organisation's Orders dashboard")]
+        public void GivenTheUserIsPresentedWithTheOrganisationsOrdersDashboard()
+        {
+            Test.Pages.Homepage.ClickOrderTile();            
+        }
+        
+        [Given(@"they choose to create a new Order")]
+        public void GivenTheyChooseToCreateANewOrder()
+        {
+            Test.Pages.OrderForm.CreateNewOrder();
+        }
+        
+        [Then(@"the new Order is presented")]
+        public void ThenTheNewOrderIsPresented()
+        {
+            Test.Pages.OrderForm.NewOrderFormDisplayed().Should().BeTrue();
+        }
+        
+        [Then(@"the User is presented with a control to return to the Organisation's Orders dashboard")]
+        public void ThenTheUserIsPresentedWithAControlToReturnToTheOrganisationSOrdersDashboard()
+        {
+            Test.Pages.OrderForm.BackLinkDisplayed();
+        }
+        
+        [Then(@"the User is unable to delete the order")]
+        public void ThenTheUserIsUnableToDeleteTheOrder()
+        {
+            Test.Pages.OrderForm.DeleteOrderButtonIsDisabled();
+        }
+        
+        [Then(@"the User is unable to preview the order summary")]
+        public void ThenTheUserIsUnableToPreviewTheOrderSummary()
+        {
+            Test.Pages.OrderForm.PreviewOrderButtonIsDisabled();
+        }
+        
+        [Then(@"the User is unable to submit the order")]
+        public void ThenTheUserIsUnableToSubmitTheOrder()
+        {
+            Test.Pages.OrderForm.SubmitOrderButtonIsDisabled();
+        }
+        
+        [Then(@"there is alt text content on the disabled Delete order button")]
+        public void ThenThereIsAltTextContentOnTheDisabledDeleteOrderButton()
+        {
+            Test.Pages.OrderForm.DeleteOrderButtonHasAltTest();
+        }
+        
+        [Then(@"there is alt text content on the disabled Preview Order Summary button")]
+        public void ThenThereIsAltTextContentOnTheDisabledPreviewOrderSummaryButton()
+        {
+            Test.Pages.OrderForm.PreviewOrderButtonHasAltTest();
+        }
+        
+        [Then(@"there is alt text content on the disabled Submit button")]
+        public void ThenThereIsAltTextContentOnTheDisabledSubmitButton()
+        {
+            Test.Pages.OrderForm.SubmitOrderButtonHasAltTest();
+        }
+    }
+}
---
+++ src/OrderFormAcceptanceTests.Tests/Features/CreateAnOrder.feature
@@ -0,0 +1,21 @@
+﻿Feature: Create An Order
+	As a Buyer
+	I want to create an Order
+	So that I can order from the Buying Catalogue
+
+Background: 
+	Given that a User is not authenticated and the user logs in using the 'login' function
+	When the User is a Buyer User
+	Then the User will be logged in
+
+Scenario: Create a new Order
+	Given the User is presented with the Organisation's Orders dashboard
+	And they choose to create a new Order
+	Then the new Order is presented
+	And the User is presented with a control to return to the Organisation's Orders dashboard
+	And the User is unable to delete the order
+	And the User is unable to preview the order summary
+	And the User is unable to submit the order
+	And there is alt text content on the disabled Delete order button
+	And there is alt text content on the disabled Preview Order Summary button
+	And there is alt text content on the disabled Submit button
