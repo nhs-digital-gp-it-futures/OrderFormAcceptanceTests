75a6ad9ed2188657d59e97929be8a5a377afbec3
Tony Lawson <tony.lawson@bjss.com>
2020-06-10 11:38:26 +0100

    moved delete inside IF in after hook

--- src/OrderFormAcceptanceTests.Steps/Steps/Hooks.cs
+++ src/OrderFormAcceptanceTests.Steps/Steps/Hooks.cs
@@ -13,15 +13,15 @@ namespace OrderFormAcceptanceTests.Steps.Steps
 
         [AfterScenario]
         public void AfterScenario()
-        {
-            var SRs = new ServiceRecipient().RetrieveByOrderId(Test.ConnectionString, ((Order)Context["CreatedOrder"]).OrderId);
-            foreach (ServiceRecipient sr in SRs)
-            {
-                sr.Delete(Test.ConnectionString);
-            }
-
+        {            
             if (Context.ContainsKey("CreatedOrder"))
             {
+                var SRs = new ServiceRecipient().RetrieveByOrderId(Test.ConnectionString, ((Order)Context["CreatedOrder"]).OrderId);
+                foreach (ServiceRecipient sr in SRs)
+                {
+                    sr.Delete(Test.ConnectionString);
+                }
+
                 ((Order)Context["CreatedOrder"]).Delete(Test.ConnectionString);
             }
             if (Context.ContainsKey("CreatedContact"))
