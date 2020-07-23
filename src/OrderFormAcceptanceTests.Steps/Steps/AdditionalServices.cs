a92ba63283a523fbbb592a307316cb012309a8cb
Simon Lawrence <sll@bjss.com>
2020-07-23 17:12:13 +0100

    8297: Manage Additional Services, view and selected AC tests

--- src/OrderFormAcceptanceTests.Steps/Steps/AdditionalServices.cs
+++ src/OrderFormAcceptanceTests.Steps/Steps/AdditionalServices.cs
@@ -6,49 +6,63 @@ using TechTalk.SpecFlow;
 
 namespace OrderFormAcceptanceTests.Steps.Steps
 {
-	[Binding]
-	public sealed class AdditionalServices : TestBase
-	{
-		public AdditionalServices(UITest test, ScenarioContext context) : base(test, context)
-		{
-		}
+    [Binding]
+    public sealed class AdditionalServices : TestBase
+    {
+        public AdditionalServices(UITest test, ScenarioContext context) : base(test, context)
+        {
+        }
 
-		[Given(@"there are no Additional Services in the order")]
-		public void GivenThereAreNoAdditionalServicesInTheOrder()
-		{
+        [Given(@"there are no Additional Services in the order")]
+        public void GivenThereAreNoAdditionalServicesInTheOrder()
+        {
             Context.Should().NotContainKey("CreatedAdditionalServiceOrderItem");
             var order = (Order)Context["CreatedOrder"];
-			var searchedOrderItem = new OrderItem().RetrieveByOrderId(Test.ConnectionString, order.OrderId, 2);
+            var searchedOrderItem = new OrderItem().RetrieveByOrderId(Test.ConnectionString, order.OrderId, 2);
             searchedOrderItem.Should().BeEmpty();
-		}
+        }
 
-		[Given(@"an Additional Service is added to the order")]
-		public void GivenAnAdditionalServiceIsAddedToTheOrder()
-		{
-			new OrderForm(Test, Context).GivenTheAdditionalServicesSectionIsComplete();
-			var orderItem = new OrderItem().GenerateAdditionalServiceOrderItemWithFlatPricedPerPatient((Order)Context["CreatedOrder"]);
-			orderItem.Create(Test.ConnectionString);
-			Context.Add("CreatedAdditionalServiceOrderItem", orderItem);
-		}
+        [Given(@"an Additional Service is added to the order")]
+        public void GivenAnAdditionalServiceIsAddedToTheOrder()
+        {
+            new OrderForm(Test, Context).GivenTheAdditionalServicesSectionIsComplete();
+            var orderItem = new OrderItem().GenerateAdditionalServiceOrderItemWithFlatPricedPerPatient((Order)Context["CreatedOrder"]);
+            orderItem.Create(Test.ConnectionString);
+            Context.Add("CreatedAdditionalServiceOrderItem", orderItem);
+        }
 
-		[StepDefinition(@"the User is able to manage the Additional Services section")]
-		[When(@"the User has chosen to manage the Additional Service section")]
-		public void ThenTheUserIsAbleToManageTheAdditionalServicesSection()
-		{
-			Test.Pages.OrderForm.ClickEditAdditionalServices();
-		}
+        [StepDefinition(@"the User is able to manage the Additional Services section")]
+        [When(@"the User has chosen to manage the Additional Service section")]
+        public void ThenTheUserIsAbleToManageTheAdditionalServicesSection()
+        {
+            Test.Pages.OrderForm.ClickEditAdditionalServices();
+            ThenTheAdditionalServiceDashboardIsPresented();
+        }
 
-		[StepDefinition(@"the Additional Service dashboard is presented")]
-		public void ThenTheAdditionalServiceDashboardIsPresented()
-		{
-			Test.Pages.AdditionalServices.PageDisplayed();
-		}
+        [StepDefinition(@"the Additional Service dashboard is presented")]
+        public void ThenTheAdditionalServiceDashboardIsPresented()
+        {
+            Test.Pages.AdditionalServices.PageDisplayed();
+        }
 
-		[Then(@"there is a control to add a Additional Service")]
-		public void ThenThereIsAControlToAddAAdditionalService()
-		{
-			Test.Pages.AdditionalServices.AddAdditionalServiceButtonDisplayed().Should().BeTrue();
-		}
+        [StepDefinition(@"the Additional Services dashboard is presented")]
+        public void ThenTheAssociatedServicesDashboardIsPresented()
+        {
+            Test.Pages.OrderForm.EditNamedSectionPageDisplayed("Additional Service").Should().BeTrue();
+        }
+
+        [StepDefinition(@"the User has chosen to manage the Additional Services section")]
+        public void WhenTheUserHasChosenToManageTheAdditionalServiceSection()
+        {
+            new CommonSteps(Test, Context).WhenTheOrderFormForTheExistingOrderIsPresented();
+            ThenTheUserIsAbleToManageTheAdditionalServicesSection();
+        }
+
+        [Then(@"there is a control to add a Additional Service")]
+        public void ThenThereIsAControlToAddAAdditionalService()
+        {
+            Test.Pages.AdditionalServices.AddAdditionalServiceButtonDisplayed().Should().BeTrue();
+        }
 
         [When(@"the User chooses to add a single Additional Service")]
         public void WhenTheUserChoosesToAddASingleAdditionalService()
@@ -60,13 +74,13 @@ namespace OrderFormAcceptanceTests.Steps.Steps
         [Then(@"the Additional Services of the Catalogue Solutions in the order is displayed")]
         public void ThenTheyArePresentedWithTheAdditionalServiceAvailableFromTheirChosenSupplier()
         {
-			Test.Pages.OrderForm.EditNamedSectionPageDisplayed("Add Additional Service").Should().BeTrue();
+            Test.Pages.OrderForm.EditNamedSectionPageDisplayed("Add Additional Service").Should().BeTrue();
         }
 
         [Then(@"they can select one Additional Service to add")]        
         public void ThenTheyCanSelectOneAdditionalServiceToAdd()
         {
-			Test.Pages.OrderForm.NumberOfRadioButtonsDisplayed().Should().BeGreaterThan(0);
+            Test.Pages.OrderForm.NumberOfRadioButtonsDisplayed().Should().BeGreaterThan(0);
         }
 
         [Given(@"the User is presented with Additional Services available from their chosen Supplier")]
@@ -131,7 +145,7 @@ namespace OrderFormAcceptanceTests.Steps.Steps
 
         [Given("the User is on the Edit Price form")]
         public void GivenIAmOnTheEditPriceForm()
-		{
+        {
             var common = new CommonSteps(Test, Context);
             common.WhenTheOrderFormForTheExistingOrderIsPresented();
             GivenTheAvailablePricesForTheSelectedAdditionalServiceArePresented();
@@ -238,6 +252,15 @@ namespace OrderFormAcceptanceTests.Steps.Steps
             GivenTheUserChoosesToEditTheSavedAdditionalService();
         }
 
+        [Given(@"there is one or more Additional Services added to the order")]
+        public void GivenTheThereAreOneOrMoreAddionalServicesAddedtoTheOrder()
+        {
+            new OrderForm(Test, Context).GivenTheAdditionalServicesSectionIsComplete();
+
+            var orderItem = new OrderItem().GenerateAdditionalServiceOrderItemWithVariablePricedPerPatient((Order)Context["CreatedOrder"]);
+            orderItem.Create(Test.ConnectionString);
+            Context.Add("CreatedAdditionalServiceOrderItem", orderItem);
+        }
 
         [StepDefinition(@"the User chooses to edit the saved Additional service")]
         public void GivenTheUserChoosesToEditTheSavedAdditionalService()
