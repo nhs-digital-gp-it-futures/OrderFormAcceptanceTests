b49788bbd805d7f251e71a78de2dfd9cfed8b921
Jonathan Alsop <j.r.alsop.18@gmail.com>
2020-07-30 10:57:39 +0100

    Add helper functions

--- src/OrderFormAcceptanceTests.TestData/SupplierInfo.cs
+++ src/OrderFormAcceptanceTests.TestData/SupplierInfo.cs
@@ -1,32 +1,55 @@
 ﻿using OrderFormAcceptanceTests.TestData.Utils;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace OrderFormAcceptanceTests.TestData
 {
-	public static class SupplierInfo
-	{
-		public static bool SupplierHasContactInfo(string connectionString, string supplierName)
-		{
-			var query = @"SELECT TOP (1000) [Id]
-				  ,[SupplierId]
-				  ,[FirstName]
-				  ,[LastName]
-				  ,[Email]
-				  ,[PhoneNumber]
-				  ,[LastUpdated]
-				  ,[LastUpdatedBy]
-			  FROM[buyingcatalogue].[dbo].[SupplierContact]
-			  Where SupplierId = (Select Id FROM Supplier s where s.Name=@supplierName)";
+    public static class SupplierInfo
+    {
+        public static bool SupplierHasContactInfo(string connectionString, string supplierName)
+        {
+            var query = @"SELECT TOP (1000) [Id]
+                  ,[SupplierId]
+                  ,[FirstName]
+                  ,[LastName]
+                  ,[Email]
+                  ,[PhoneNumber]
+                  ,[LastUpdated]
+                  ,[LastUpdatedBy]
+              FROM[buyingcatalogue].[dbo].[SupplierContact]
+              Where SupplierId = (Select Id FROM Supplier s where s.Name=@supplierName)";
 
-			try
-			{
-				Contact contact = SqlExecutor.Execute<Contact>(connectionString, query, new { supplierName }).First();
-				return true;
-			}
-			catch
-			{
-				return false;
-			}
-		}
-	}
+            try
+            {
+                Contact contact = SqlExecutor.Execute<Contact>(connectionString, query, new { supplierName }).First();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static IEnumerable<string> SuppliersWithAdditionalServices(string connectionString, ProvisioningType provisioningType)
+        {
+            return SupplierLookup(connectionString, CatalogueItemType.AdditionalService, provisioningType);
+        }
+
+        public static IEnumerable<string> SuppliersWithAssociatedServices(string connectionString, ProvisioningType provisioningType)
+        {
+            return SupplierLookup(connectionString, CatalogueItemType.AssociatedService, provisioningType);
+        }
+
+        private static IEnumerable<string> SupplierLookup(string connectionString, CatalogueItemType catalogueItemType, ProvisioningType provisioningType)
+        {
+            var query = @"SELECT ci.[CatalogueItemId]      
+                            FROM [buyingcatalogue].[dbo].[CatalogueItem] as ci
+                            INNER JOIN CataloguePrice as pr ON ci.CatalogueItemId=pr.CatalogueItemId
+                            WHERE ci.CatalogueItemTypeId=@catalogueItemType
+                            AND pr.ProvisioningTypeId=@provisioningType";
+            
+            return SqlExecutor.Execute<string>(connectionString, query, new { catalogueItemType = (int)catalogueItemType, provisioningType = (int)provisioningType });
+        }
+    }
 }
