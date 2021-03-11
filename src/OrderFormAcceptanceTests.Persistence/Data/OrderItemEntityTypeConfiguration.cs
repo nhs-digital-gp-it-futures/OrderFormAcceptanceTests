1802161abc5d6c0d700363efeec45a685e1babb7
PhysicsJon <j.r.alsop.18@gmail.com>
2021-03-11 10:40:18 +0000

    Add persistence project

---
+++ src/OrderFormAcceptanceTests.Persistence/Data/OrderItemEntityTypeConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderFormAcceptanceTests.Domain;
+
+namespace OrderFormAcceptanceTests.Persistence.Data
+{
+    internal sealed class OrderItemEntityTypeConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            const string catalogueItemId = "CatalogueItemId";
+
+            builder.ToTable("OrderItem");
+            builder.Property<CatalogueItemId>(catalogueItemId)
+                .HasMaxLength(14)
+                .HasConversion(id => id.ToString(), id => CatalogueItemId.ParseExact(id));
+
+            builder.Property(o => o.OrderId).IsRequired();
+            builder.HasKey(nameof(OrderItem.OrderId), catalogueItemId);
+
+            builder.Property(i => i.Created).HasDefaultValueSql("(GetUtcDate())");
+            builder.Property(i => i.CurrencyCode)
+                .IsRequired()
+                .HasMaxLength(3);
+
+            builder.Property(i => i.DefaultDeliveryDate).HasColumnType("date");
+            builder.Property(i => i.LastUpdated).HasDefaultValueSql("(GetUtcDate())");
+            builder.Property(i => i.Price).HasColumnType("decimal(18, 4)");
+
+            builder.HasOne(i => i.CatalogueItem)
+                .WithMany()
+                .HasForeignKey(catalogueItemId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_OrderItem_CatalogueItem");
+
+            builder
+                .Property(i => i.CataloguePriceType)
+                .HasConversion<int>()
+                .HasColumnName("CataloguePriceTypeId");
+
+            builder
+                .Property(i => i.EstimationPeriod)
+                .HasConversion<int>()
+                .HasColumnName("EstimationPeriodId");
+
+            builder.HasOne<Order>()
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(o => o.OrderId)
+                .HasConstraintName("FK_OrderItem_Order");
+
+            builder.HasOne(i => i.PricingUnit)
+                .WithMany()
+                .HasForeignKey("PricingUnitName")
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_OrderItem_PricingUnit");
+
+            builder
+                .Property(i => i.ProvisioningType)
+                .HasConversion<int>()
+                .HasColumnName("ProvisioningTypeId");
+
+            builder
+                .Property(i => i.PriceTimeUnit)
+                .HasConversion<int>()
+                .HasColumnName("TimeUnitId");
+        }
+    }
+}
---
+++ src/OrderFormAcceptanceTests.Persistence/Data/PricingUnitEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderFormAcceptanceTests.Domain;
+
+namespace OrderFormAcceptanceTests.Persistence.Data
+{
+    internal sealed class PricingUnitEntityTypeConfiguration : IEntityTypeConfiguration<PricingUnit>
+    {
+        public void Configure(EntityTypeBuilder<PricingUnit> builder)
+        {
+            builder.ToTable("PricingUnit");
+            builder.HasKey(u => u.Name);
+            builder.Property(u => u.Name).HasMaxLength(20);
+            builder.Property(u => u.Description)
+                .IsRequired()
+                .HasMaxLength(40);
+        }
+    }
+}
